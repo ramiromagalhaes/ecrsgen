92128919676270908749ae6a41ad66164f9976b1
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-07-15 00:02:29 -0300

    Updating some parameters of the Haar wavelet generator.

--- haargen.cpp
+++ haargen.cpp
@@ -35,14 +35,24 @@ int main(int argc, char * args[])
      * 5) all rectangles in a HW have the same size
      * 6) no rectangles smaller than 3x3
      */
-    std::vector<float> weights(2); // will vary depending on the amount of rects
-    weights[0] = 1;
-    weights[1] = -1;
+    std::vector<float> weights[3]; // will vary depending on the amount of rects
+    weights[0].resize(2);
+    weights[0][0] = -1;
+    weights[0][1] = 1;
+    weights[1].resize(3);
+    weights[1][0] = -1;
+    weights[1][1] = 2;
+    weights[1][2] = -1;
+    weights[2].resize(4);
+    weights[2][0] = -1;
+    weights[2][1] = 1;
+    weights[2][2] = -1;
+    weights[2][3] = 1;
 
     //First, let's produce simple haar wavelets with 2 features
-    for(int w = 1; w <= SAMPLE_SIZE / MIN_RECT_SIZE; w++) //width of both rectangles.
+    for(int w = 1; w <= SAMPLE_SIZE; w++) //width of both rectangles.
     {
-        for(int h = 1; h <= SAMPLE_SIZE / MIN_RECT_SIZE; h++) //height of both rectangles
+        for(int h = 1; h <= SAMPLE_SIZE; h++) //height of both rectangles
         {
             if ( w * h < MIN_RECT_SIZE * MIN_RECT_SIZE ) //Check against Pavani's restriction #6
             {
@@ -51,9 +61,9 @@ int main(int argc, char * args[])
             int x[MAX_DIMENSIONS], //x and y positions of each rectangle.
                 y[MAX_DIMENSIONS];
 
-            for(x[0] = 0; x[0] < SAMPLE_SIZE - MIN_RECT_SIZE; x[0]++) //for each x...
+            for(x[0] = 0; x[0] <= SAMPLE_SIZE - w; x[0]++) //for each x...
             {
-                for(y[0] = 0; y[0] < SAMPLE_SIZE - MIN_RECT_SIZE; y[0]++) //...and y of the first rectangle...
+                for(y[0] = 0; y[0] <= SAMPLE_SIZE - h; y[0]++) //...and y of the first rectangle...
                 {
                     for(int k = 1; k < MAX_DIMENSIONS; k++) //...create Haar wavelets of 1 + k dimensions...
                     {
@@ -64,9 +74,9 @@ int main(int argc, char * args[])
 
                         for(int d = 0; d < k; d++) //...where the rectangle d + 1 is (px, py) pixels away from rectangle's d upper left corner...
                         {
-                            for(dx[d] = 0; dx[d] <= SAMPLE_SIZE/MIN_RECT_SIZE; dx[d]++) //dx displacement of rectangle d + 1 in regard to d
+                            for(dx[d] = 0; dx[d] < SAMPLE_SIZE/w; dx[d]++) //dx displacement of rectangle d + 1 in regard to d
                             {
-                                for(dy[d] = 0; dy[d] <= SAMPLE_SIZE/MIN_RECT_SIZE; dy[d]++)  //dy displacement of rectangle d + 1 in regard to d
+                                for(dy[d] = 0; dy[d] < SAMPLE_SIZE/h; dy[d]++)  //dy displacement of rectangle d + 1 in regard to d
                                 {
                                     bool overlap = false;
                                     for (int i = 0; i < k; i++) //...as long as no rectangles overlap...
@@ -102,12 +112,13 @@ int main(int argc, char * args[])
                                         continue;
                                     }
 
+                                    //...then create the wavelet.
                                     std::vector<cv::Rect> rects(1 + k);
                                     for (int i = 0; i <= k; i++)
                                     {
                                         rects[i] = cv::Rect(x[i], y[i], w, h);
                                     }
-                                    HaarWavelet wavelet(&sampleSize, &position, rects, weights);
+                                    HaarWavelet wavelet(&sampleSize, &position, rects, weights[k - 1]);
                                     wavelet.write(waveletStorage);
                                     generetedWaveletCounter++;
                                 }
