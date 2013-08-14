8ec9edc7c935e097f6afed25244a0a2f64568292
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-08-13 21:01:30 -0300

    Adding some beta code to the detector class.

--- lib/detector.cpp
+++ lib/detector.cpp
@@ -1,5 +1,44 @@
 #include "detector.h"
 
+
+
 Detector::Detector()
 {
+}
+
+
+
+bool Detector::setSubwindow(const int left, const int top)
+{
+    if( left < 0 || top < 0 ||
+        left + defaultSize.width >= image.cols ||
+        top + defaultSize.height >= image.rows )
+    {
+        return false;
+    }
+
+    currentRoi.x = left;
+    currentRoi.y = top;
+
+    {
+        const cv::Rect integralsRoi(currentRoi.x, currentRoi.y, currentRoi.width + 1, currentRoi.height + 1);
+        currentIntegralImage = integralImage(integralsRoi);
+        currentSquareIntegralImage = squareIntegralImage(integralsRoi);
+    }
+
+    const double area = currentRoi.area();
+    subwindowMean = (currentIntegralImage.at<int>(currentRoi.width, currentRoi.height) - currentIntegralImage.at<int>(0, 0)) / area;
+    subwindowStdDeviation = sqrt(
+        subwindowMean * subwindowMean - (currentSquareIntegralImage.at<int>(currentRoi.width, currentRoi.height) - currentSquareIntegralImage.at<int>(0, 0)) / area
+    );
+
+//    const double normalized = (pixel - subwindowMean)/sqrt(subwindowVariance);
+//    const double normalized_on_gray = (CHAR_MAX/2) + normalized;
+}
+
+
+
+bool Detector::setSubwindow(const cv::Point topLeftPosition)
+{
+    setSubwindow(topLeftPosition.x, topLeftPosition.y);
 }
--- lib/detector.h
+++ lib/detector.h
@@ -8,8 +8,55 @@ class Detector
 public:
     Detector();
 
+    bool setSubwindow(const int left, const int top);
+    bool setSubwindow(const cv::Point topLeftPosition);
+
 private:
-    std::vector<HaarWavelet> wavelets;
+    /**
+     * @brief image the whole image over which the detector iterates over.
+     */
+    const cv::Mat image;
+
+    /**
+     * @brief image the integral image calculated over the image.
+     */
+    const cv::Mat integralImage;
+
+    /**
+     * @brief image the square integral image calculated over the image.
+     */
+    const cv::Mat squareIntegralImage;
+
+    /**
+     * @brief defaultSize the size of the detector when it is scaled by 1. Typically it is the size the detector is supposed to be during trainning.
+     */
+    const cv::Size defaultSize;
+
+    /**
+     * @brief currentRoi stores both the current size and position of the scanning window.
+     */
+    cv::Rect currentRoi;
+
+    /**
+     * @brief currentSubwindow references the integral image data that's being scanned.
+     */
+    cv::Mat currentIntegralImage;
+
+    /**
+     * @brief currentSubwindow references the square integral image data that's being scanned.
+     */
+    cv::Mat currentSquareIntegralImage;
+
+    /**
+     * @brief subwindowMean The mean value of the pixels in the subwindow.
+     */
+    double subwindowMean;
+
+    /**
+     * @brief subwindowStdDeviation The standard deviation of the pixels in the subwindow.
+     */
+    double subwindowStdDeviation;
+
 };
 
 #endif // DETECTOR_H
